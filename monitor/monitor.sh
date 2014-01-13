55dabc7fcab9db9b56eeb6df52baa487c4fa48e5
Christian Roser <christian.roser@1und1.de>
2014-01-13 15:20:15 +0100

    neneuu

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -1,9 +1,9 @@
   
 
-xrandr --output DFP1 --auto --rotate left
-xrandr --output DFP5 --auto --rotate left
-xrandr --output DFP6 --auto --rotate left
+xrandr --output HDMI-0 --auto --rotate left #links
+xrandr --output DisplayPort-0 --auto --rotate left #mitte
+xrandr --output DVI-0 --auto --rotate left #ganz rechts
   
-xrandr --output DFP5 --auto --right-of DFP1
-xrandr --output DFP6 --auto --right-of DFP5
-herbstclient move_monitor 0 3150x1680+0+0
+xrandr --output DisplayPort-0 --auto --right-of HDMI-0
+xrandr --output DVI-0 --auto --right-of DisplayPort-0
+herbstclient move_monitor 0 3600x1920+0+0
