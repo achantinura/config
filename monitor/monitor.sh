039d5bd3fcee5b058208956db6ab5af51dbe65da
Christian Roser <christian.roser@1und1.de>
2014-02-21 13:44:29 +0100

    changed config

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -1,9 +1,13 @@
+xrandr --output HDMI-0 --auto --rotate left #links
+xrandr --output DisplayPort-0 --auto --rotate left #mitte
+# for leaving this monitor horizontal comment next line
+xrandr --output DVI-0 --auto --rotate left #ganz rechts
   
+xrandr --output DisplayPort-0 --auto --right-of HDMI-0
+xrandr --output DVI-0 --auto --right-of DisplayPort-0
+#herbstclient move_monitor 0 2400x1920+0+0
+#xrandr --output DVI-0 --auto --pos 2400x300 #ganz rechts
+#herbstclient add_monitor 1920x1200+2400+300 9 second
+#herbstclient lock_tag second
 
-xrandr --output DFP1 --auto --rotate left
-xrandr --output DFP5 --auto --rotate left
-xrandr --output DFP6 --auto --rotate left
-  
-xrandr --output DFP5 --auto --right-of DFP1
-xrandr --output DFP6 --auto --right-of DFP5
-herbstclient move_monitor 0 3150x1680+0+0
+herbstclient move_monitor 0 3600x1920+0+0
