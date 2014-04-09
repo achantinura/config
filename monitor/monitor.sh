9c1f4e459cb019245a29204d9b32999c6c7fd4ca
croser <croser@croser-Precision-T1700.(none)>
2014-04-09 13:22:11 +0200

    modified

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -1,10 +1,10 @@
 xrandr --output HDMI-0 --auto --rotate left #links
-xrandr --output DisplayPort-0 --auto --rotate left #mitte
+xrandr --output DisplayPort-0 --auto --rotate left #rechts
 # for leaving this monitor horizontal comment next line
-xrandr --output DVI-0 --auto --rotate left #ganz rechts
+xrandr --output DVI-0 --auto --rotate left #mitte
   
-xrandr --output DisplayPort-0 --auto --right-of HDMI-0
-xrandr --output DVI-0 --auto --right-of DisplayPort-0
+xrandr --output DVI-0 --auto --right-of HDMI-0
+xrandr --output DisplayPort-0 --auto --right-of DVI-0
 #herbstclient move_monitor 0 2400x1920+0+0
 #xrandr --output DVI-0 --auto --pos 2400x300 #ganz rechts
 #herbstclient add_monitor 1920x1200+2400+300 9 second
